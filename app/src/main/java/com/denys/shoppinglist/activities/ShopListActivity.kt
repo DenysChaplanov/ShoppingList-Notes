84a3a07ba56987b68ba129b30973391ea8f02910
Denys <chaplanov.den@gmail.com>
2024-12-26 13:38:47 +0100

    Lesson32
    Options menu in ShopListActivity

--- app/src/main/java/com/denys/shoppinglist/activities/ShopListActivity.kt
+++ app/src/main/java/com/denys/shoppinglist/activities/ShopListActivity.kt
@@ -2,6 +2,8 @@ package com.denys.shoppinglist.activities
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.view.MenuItem.OnActionExpandListener
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.denys.shoppinglist.R
@@ -12,6 +14,7 @@ import com.denys.shoppinglist.entities.ShopListNameItem
 class ShopListActivity : AppCompatActivity() {
     private lateinit var binding: ActivityShopListBinding
     private var shopListNameItem: ShopListNameItem? = null
+    private lateinit var saveItem: MenuItem
 
     private val mainViewModel: MainViewModel by viewModels {
         MainViewModel.MainViewModelFactory((applicationContext as MainApp).database)
@@ -27,7 +30,27 @@ class ShopListActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.shop_list_menu, menu)
+        saveItem = menu?.findItem(R.id.save_item)!!
+        saveItem.isVisible = false
+        val newItem = menu.findItem(R.id.new_item)
+        newItem.setOnActionExpandListener(expandActionView())
         return true
+    }
+
+    private fun expandActionView(): OnActionExpandListener{
+        return object : OnActionExpandListener{
+            override fun onMenuItemActionExpand(item: MenuItem): Boolean {
+                saveItem.isVisible = true
+                return true
+            }
+
+            override fun onMenuItemActionCollapse(item: MenuItem): Boolean {
+                saveItem.isVisible = false
+                invalidateOptionsMenu()
+                return true
+            }
+
+        }
     }
 
     private fun init(){
