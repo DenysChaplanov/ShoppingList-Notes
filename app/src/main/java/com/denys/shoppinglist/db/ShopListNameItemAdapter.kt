c76a058577e2ca3cb18097c44212a930a3d8228c
Denys <chaplanov.den@gmail.com>
2024-12-26 14:02:52 +0100

    Lesson33
    Creating ShopListItemAdapter #1

---
+++ app/src/main/java/com/denys/shoppinglist/db/ShopListNameItemAdapter.kt
@@ -0,0 +1,76 @@
+package com.denys.shoppinglist.db
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.denys.shoppinglist.R
+import com.denys.shoppinglist.databinding.ListNameItemBinding
+import com.denys.shoppinglist.entities.ShopListNameItem
+import com.denys.shoppinglist.entities.ShoppingListItem
+
+class ShopListNameItemAdapter(private val listener: Listener) : ListAdapter<ShoppingListItem,
+        ShopListNameItemAdapter.ItemHolder>(ItemComparator()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
+        return if (viewType == 0) {
+            ItemHolder.createShopItem(parent)
+        } else {
+            ItemHolder.createLibraryItem(parent)
+        }
+    }
+
+    override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+        if(getItem(position).itemType == 0) {
+            holder.setItemData(getItem(position), listener)
+        } else {
+            holder.setLibraryData(getItem(position), listener)
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return getItem(position).itemType
+    }
+
+    class ItemHolder(view: View) : RecyclerView.ViewHolder(view){
+        private val binding = ListNameItemBinding.bind(view)
+
+        fun setItemData(shopListItem: ShoppingListItem, listener: Listener) = with(binding){
+
+        }
+        fun setLibraryData(shopListItem: ShoppingListItem, listener: Listener) = with(binding){
+
+        }
+
+        companion object {
+            fun createShopItem(parent: ViewGroup): ItemHolder{
+                return ItemHolder(
+                    LayoutInflater.from(parent.context).
+                    inflate(R.layout.list_name_item, parent, false))
+            }
+            fun createLibraryItem(parent: ViewGroup): ItemHolder{
+                return ItemHolder(
+                    LayoutInflater.from(parent.context).
+                    inflate(R.layout.list_name_item, parent, false))
+            }
+        }
+    }
+
+    class ItemComparator: DiffUtil.ItemCallback<ShoppingListItem>(){
+        override fun areItemsTheSame(oldItem: ShoppingListItem, newItem: ShoppingListItem): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: ShoppingListItem, newItem: ShoppingListItem): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    interface Listener{
+        fun deleteItem(id: Int)
+        fun editItem(shopListName: ShopListNameItem)
+        fun onClickItem(shopListNameItem: ShopListNameItem)
+    }
+}
