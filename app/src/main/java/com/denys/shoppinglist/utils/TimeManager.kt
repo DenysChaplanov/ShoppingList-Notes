2b4a70abfcc45a56125a824543ec7099428ed338
Denys <chaplanov.den@gmail.com>
2025-01-02 20:05:48 +0100

    Section 4: PreferenceScreen. Lesson49
    Creating PreferenceScreen #1

--- app/src/main/java/com/denys/shoppinglist/utils/TimeManager.kt
+++ app/src/main/java/com/denys/shoppinglist/utils/TimeManager.kt
@@ -6,7 +6,7 @@ import java.util.Locale
 
 object TimeManager {
     fun getCurrentTime(): String {
-        val formatter = SimpleDateFormat("hh:mm:ss - yyyy/MM/dd", Locale.getDefault())
+        val formatter = SimpleDateFormat("hh:mm:ss — yyyy/MM/dd", Locale.getDefault())
         return formatter.format(Calendar.getInstance().time)
     }
 }
